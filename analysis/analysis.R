4213d4a639951e8bb508239c419c403309c661db
Rafael Gomes Mantovani <rgmantovani@gmail.com>
2023-04-26 12:45:06 -0300

    generating data for Friedman-Nemenyi

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -1,20 +1,28 @@
 # ---------------------------------------------
-# Reading data
+# Required Packages
 # ---------------------------------------------
 
 library(reshape2)
+library(ggplot2)
+library(mlr)
 
-data1 = read.csv("../dataset_1_results.csv")
+# ---------------------------------------------
+# Reading data
+# ---------------------------------------------
+
+
+
+data1 = read.csv("../results/dataset_1_results.csv")
 df1   = melt(data1, id.vars = 1)
 df1$dataset = "dataset1"
 
 
-data2 = read.csv("../dataset_2_results.csv")
+data2 = read.csv("../results/dataset_2_results.csv")
 df2   = melt(data2, id.vars = 1)
 df2$dataset = "dataset2"
 
 
-data3 = read.csv("../dataset_3_results.csv")
+data3 = read.csv("../results/dataset_3_results.csv")
 df3   = melt(data3, id.vars = 1)
 df3$dataset = "dataset3"
 
@@ -33,18 +41,51 @@ df.full$algo = factor(df.full$algo)
 # Boxplot
 # ---------------------------------------------
 
-library(ggplot2)
-
 g = ggplot(df.full, aes(x = reorder(algo, -acc), y = acc, group = algo))
 g = g + geom_violin() + geom_boxplot(width = .2)
-g = g + facet_grid(~dataset)
+g = g + facet_grid(~dataset) + theme_bw()
 g = g + labs(x = "Algorithm", y = "Accuracy")
 g = g + theme(axis.text.x=element_text(angle = 90, hjust = 1))
-ggsave(g, filename = "boxplot.pdf", width = 8.03, height = 3.05)
-# Saving 8.03 x 3.05 in image
+ggsave(g, filename = "boxplot.pdf", width = 7.55, height = 3.16)
+
+# ---------------------------------------------
+# RF plot using the best dataset (dataset2)
+# ---------------------------------------------
 
 
+# TODO: generate dataset
+# cat(" @ Plot: Random Forest (importance) \n")
 
+# dataset = read.csv()
+# mlrTask = mlr::makeClassifTask(df_all2_resc[,-1], id = "test", target = "rotulo")
+# lrn     = mlr::makeLearner("classif.ranger", importance = "permutation")
+# model   = mlr::train(task = mlrTask, learner = lrn)
+
+# trueModel = model$learner.model
+
+# importance = as.data.frame(trueModel$variable.importance)
+# rf.df = cbind(rownames(importance), importance)
+# rownames(rf.df) = NULL
+# colnames(rf.df) = c("Feature", "Importance")
+
+# g_importance = ggplot(rf.df, aes(x = reorder(Feature, Importance), y = Importance))
+# g_importance = g_importance  + geom_col(width = 0.8, fill="lightblue", col="darkblue")
+# g_importance = g_importance  + labs(y="Importance", x = "Feature") + coord_flip() + theme_bw() 
+# # g_importance # for debug
+# ggsave(g_importance, file = "plots/fig_randomForest.pdf", units = "in", width = 9, 
+# 	height = 6, dpi = 300, pointsize = 20)
+
+
+# ---------------------------------------------
+# Data frame for statistical validation
+# ---------------------------------------------
+
+df.friedman = rbind(data1, data2, data3)
+df.friedman = df.friedman[,-1]
+write.csv(df.friedman, file = "../data/dataForFriedman.csv")
+
+# https://github.com/gabrieljaguiar/nemenyi
+# python3 nemenyi.py examples/dataForFriedman.csv cropOutput.tex --h
 
 # ---------------------------------------------
 # ---------------------------------------------
