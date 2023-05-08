0b8363136167a75dbfdb42fa3d6d3bfdd2d99e5b
Rafael Gomes Mantovani <rgmantovani@gmail.com>
2023-05-08 15:55:10 -0300

    removing mlp results, because they overfitted

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -2,6 +2,9 @@
 # Required Packages
 # ---------------------------------------------
 
+cat(" - Loading required packages\n")
+set.seed(42)
+
 library(reshape2)
 library(ggplot2)
 library(mlr)
@@ -10,18 +13,28 @@ library(mlr)
 # Reading data
 # ---------------------------------------------
 
-ids = 1:nrow(data1)
+cat(" - Reading results' data\n")
+
 data1 = read.csv("../results/accuracies_dataset_1.csv")
+ids = 1:nrow(data1)
+
 data1 = cbind(ids, data1)
+# ml does not work in the problem (it needs tuning)
+data1$mlp = NULL 
+
 df1   = melt(data1, id.vars = 1) 
 df1$dataset = "dataset1"
 
 data2 = read.csv("../results/accuracies_dataset_2.csv")
+# ml does not work in the problem (it needs tuning)
+data2$mlp = NULL 
 data2 = cbind(ids, data2)
 df2   = melt(data2, id.vars = 1)
 df2$dataset = "dataset2"
 
 data3 = read.csv("../results/accuracies_dataset_3.csv")
+# ml does not work in the problem (it needs tuning)
+data3$mlp = NULL 
 data3 = cbind(ids, data3)
 df3   = melt(data3, id.vars = 1)
 df3$dataset = "dataset3"
@@ -33,6 +46,8 @@ df.full$algo = as.factor(toupper(as.character(df.full$algo)))
 # ---------------------------------------------
 # Boxplot
 # ---------------------------------------------
+
+cat(" - Plot: boxplot + violin plot\n")
 
 g = ggplot(df.full, aes(x = reorder(algo, -acc), y = acc, group = algo))
 g = g + geom_violin() + geom_boxplot(width = .2)
@@ -46,11 +61,14 @@ ggsave(g, filename = "boxplot.pdf", width = 7.55, height = 3.16)
 # Predictions
 # ---------------------------------------------
 
-pred2 = read.csv("../results/predictions_dataset_2.csv")
-pred2$dataset = "dataset2"
-mpred2 = melt(pred2, id.vars = c(1, 9))
+cat(" - Loading predictions obtained in dataset 1\n")
 
-pred.full = mpred2
+pred2 = read.csv("../results/predictions_dataset_1.csv")
+pred2$dataset = "dataset1"
+# ml does not work in the problem (it needs tuning)
+pred2$MLP = NULL
+
+pred.full = melt(pred2, id.vars = c(1, 8))
 pred.full$value = as.factor(pred.full$value)
 zero.ids = which(pred.full$value == 0)
 
@@ -63,11 +81,13 @@ g2 = g2 + scale_fill_manual(values = c("lightgrey", "black"))
 g2 = g2 + scale_colour_manual(values = c("lightgrey", "black"))
 g2 = g2 + theme(axis.text.x = element_blank(), axis.ticks = element_blank())
 g2 = g2 + labs(x = "Image index", y = "Algorithm", fill = "Class", colour = "Class")
-ggsave(g2, filename = "predictionsPlot.pdf", width = 7.55, height = 2.44)
+ggsave(g2, filename = "predictionsPlot_dataset1.pdf", width = 7.55, height = 2.44)
 
 # ---------------------------------------------
 # Overall performance
 # ---------------------------------------------
+
+cat(" - Computing overall performance values \n")
 
 avg1 = apply(data1[,-1], 2, mean)
 sd1  = apply(data1[,-1], 2, sd)
@@ -92,9 +112,11 @@ write.csv(df.perf, file = "../data/overallPerformances.csv")
 # Data frame for statistical validation
 # ---------------------------------------------
 
+cat(" - Exporting data for statistical tests (Friedman) \n")
+
 df.friedman = rbind(data1, data2, data3)
 df.friedman = df.friedman[,-1]
-write.csv(df.friedman, file = "../data/dataForFriedman.csv")
+write.csv(df.friedman, file = "../data/dataForFriedman.csv", row.names = FALSE)
 
 # https://github.com/gabrieljaguiar/nemenyi
 # python3 nemenyi.py examples/dataForFriedman.csv cropOutput.tex --h
@@ -103,9 +125,11 @@ write.csv(df.friedman, file = "../data/dataForFriedman.csv")
 # Wilcoxon per dataset
 # ---------------------------------------------
 
-cat(" *** Statistical Evaluations *** ")
+cat(" - Statistical Evaluations (Wilxocon) \n")
+
 obj1 = suppressWarnings(wilcox.test(x = data1$svc, y = data1$rf, conf.level = 0.95))
-print(obj1$p.value)
+cat("P-value: ", obj1$p.value, "\n")
+
 cat("@ SVM vs RF - Dataset1: ")
 if(obj1$p.value < 0.05) {
 	cat("There is statistical difference between the methods!\n")
@@ -114,7 +138,7 @@ if(obj1$p.value < 0.05) {
 }
 
 obj2 = suppressWarnings(wilcox.test(x = data2$svc, y = data2$rf, conf.level = 0.95))
-print(obj2$p.value)
+cat("P-value: ", obj2$p.value, "\n")
 cat("@ SVM vs RF - Dataset2: ")
 if(obj2$p.value < 0.05) {
 	cat("There is statistical difference between the methods!\n")
@@ -123,7 +147,7 @@ if(obj2$p.value < 0.05) {
 }
 
 svc1 = suppressWarnings(wilcox.test(x = data1$svc, y = data2$svc, conf.level = 0.95))
-print(svc1$p.value)
+cat("P-value: ", svc1$p.value, "\n")
 cat("@ SVM - Dataset1 vs SVM - Dataset2: ")
 if(svc1$p.value < 0.05) {
 	cat("There is statistical difference between the methods!\n")
@@ -131,14 +155,19 @@ if(svc1$p.value < 0.05) {
 	cat("There is no statistical difference between the methods!\n")
 }
 
-rf1  = suppressWarningswilcox.test(x = data1$rf, y = data2$rf, conf.level = 0.95))
-print(rf1$p.value)
+rf1  = suppressWarnings(wilcox.test(x = data1$rf, y = data2$rf, conf.level = 0.95))
+cat("P-value: ", rf1$p.value, "\n")
 cat("@ RF - Dataset1 vs RF - Dataset2: ")
 if(rf1$p.value < 0.05) {
 	cat("There is statistical difference between the methods!\n")
 } else {
 	cat("There is no statistical difference between the methods!\n")
 }
+
+# ---------------------------------------------
+# ---------------------------------------------
+
+cat(" - Finished \n")
 
 # ---------------------------------------------
 # ---------------------------------------------
