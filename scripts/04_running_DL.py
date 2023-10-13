ca9819ce9e58a08926def41202abc1922d6be8f5
Rafael Gomes Mantovani <rgmantovani@gmail.com>
2023-10-13 19:14:58 -0300

    fixing CNN script

--- scripts/04_running_DL.py
+++ scripts/04_running_DL.py
@@ -49,45 +49,62 @@ for i in range(0, len(df_testing)):
 	img = Image.open(example["im_path"]) #.getdata()
 	# converting image to numpy array
 	test_images.append(np.array(img))
-
 test_images = np.array(test_images)
 # >>> test_images.shape
 # (863, 64, 64, 3)
 
 # ----------------------------
+# Normalize pixel values to be between 0 and 1
 # ----------------------------
 
-# Normalize pixel values to be between 0 and 1
 train_images, test_images = train_images / 255.0, test_images / 255.0
 
 # ----------------------------
+# Labels from training and testing folds
 # ----------------------------
 
 train_labels = np.array( df_training["Y"])
 test_labels  = np.array( df_testing["Y"])
 
 # ----------------------------
+# Defining the prediction model (CNN)
 # ----------------------------
 
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(64, 64, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Dropout(0.25))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Dropout(0.25))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(1, activation="softmax"))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(1, activation="sigmoid"))
 
 
 model.compile(optimizer='adam',
               loss=tf.keras.losses.BinaryCrossentropy(),
               metrics=['binary_accuracy', 'accuracy'])
 
-history = model.fit(train_images, train_labels, epochs=10, 
+# ----------------------------
+# Traninig the algorithm
+# ----------------------------
+
+history = model.fit(train_images, train_labels, epochs=100, 
                     validation_data=(test_images, test_labels))
 
-test = model.evaluate(test_images,  test_labels, verbose=2)
+
+# ----------------------------
+# Evaluating predictions
+# ----------------------------
+
+test = model.evaluate(test_images, test_labels, verbose=2)
+
+predictions = model.predict(test_images)
+print(predictions)
+
 
 # --------------------------------------------------------------------------------------------------------------------
 # --------------------------------------------------------------------------------------------------------------------
